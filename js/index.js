049c3f5f306fd5101c860135c19f30875f01dcf9
SherzodbekMalikov <68462735+SherzodbekMalikov@users.noreply.github.com>
2023-05-01 01:11:05 +0500

    sidebar

--- js/index.js
+++ js/index.js
@@ -35,3 +35,5 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+/***********************/
